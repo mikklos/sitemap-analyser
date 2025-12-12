dc7265374b358f61c85d36988e9a1384a5544a28
mikklos <mikaeljansson@hotmail.com>
2025-12-12 12:22:42 +0100

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,1 +1,1 @@
-(function(){var u='https://din-dom%C3%A4n.se/sitemap-tool.js?'+Date.now();var s=document.createElement('script');s.src=u;s.onerror=function(){alert('Kunde inte ladda: '+u)};document.head.appendChild(s)})()
+(){var u='https://din-dom%C3%A4n.se/sitemap-tool.js?'+Date.now();var s=document.createElement('script');s.src=u;s.onerror=function(){alert('Kunde inte ladda: '+u)};document.head.appendChild(s)})()
