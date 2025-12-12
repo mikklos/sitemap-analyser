3e1ab9a13e502395e1025235b13d69aa235e2e10
mikklos <mikaeljansson@hotmail.com>
2025-12-12 12:27:54 +0100

    Update and rename sitemap.js to sitemap-tool.js

--- sitemap.js
+++
@@ -1,1 +0,0 @@
-(){var u='https://din-dom%C3%A4n.se/sitemap-tool.js?'+Date.now();var s=document.createElement('script');s.src=u;s.onerror=function(){alert('Kunde inte ladda: '+u)};document.head.appendChild(s)})()
